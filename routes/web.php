ff466327896498ef570e5a0716fef3ebdc3c2f54
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-06-30 19:24:20 +0200

    commit es ok

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data=[ 
+        "msg" => "benvenuto in Laravel"
+    ];
+
+
+    return view('home', $data);
 });
